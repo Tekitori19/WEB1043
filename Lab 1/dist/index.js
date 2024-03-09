1f91eeebf2e4a9abbc5e10bc205d115ce5bb55f0
Dwcks Dinh <corclan19@gmail.com>
2024-03-09 09:23:45 +0700

    Done Lab 2

---
+++ Lab 1/dist/index.js
@@ -0,0 +1,41 @@
+"use strict";
+//BT 1
+var ten = "Định";
+var age = 21;
+var hienThi = "Xin ch\u00E0o, t\u00F4i l\u00E0 ".concat(ten, " v\u00E0 t\u00F4i ").concat(age, " tu\u1ED5i.");
+// console.log(hienThi);
+//BT 2
+var num = 0.873457689;
+if (!isNaN(num)) {
+    // console.log("a không phải là số");
+}
+var convertNum = (num.toFixed(3)).toString();
+// console.log(convertNum, typeof convertNum);
+var convertNum1 = (num.toFixed(5)).toString();
+// console.log(num.toFixed(2), num.toFixed(5));
+//BT 3
+var sinhVien = "Sinh viên Công nghệ thông tin";
+var truongHoc = "FPT Polytechnic";
+// console.log(sinhVien.substring(0, 4), truongHoc.substring(0, 4));
+// console.log(sinhVien.concat(" - ", truongHoc));
+//BT 4
+var tinhTongHop = function () {
+    var so1 = document.getElementById("num1").valueAsNumber;
+    var so2 = document.getElementById("num2").valueAsNumber;
+    var content = document.getElementById("content");
+    console.log(so1, so2);
+    content.innerHTML +=
+        "T\u1ED5ng: ".concat(so1 + so2, " <br> Hi\u1EC7u: ").concat(so1 - so2, " <br>\n        T\u00EDch: ").concat(so1 * so2, " <br> Th\u01B0\u01A1ng: ").concat(so1 / so2, " <br>\n       ");
+};
+//BT 5
+var diemWEB1 = [6, 7, 9, 10, 5];
+var diemTb = function (diems) {
+    var tong = 0;
+    for (var _i = 0, diems_1 = diems; _i < diems_1.length; _i++) {
+        var diem = diems_1[_i];
+        tong += diem;
+    }
+    console.log("Điểm trung bình của lớp là: ", tong);
+};
+diemTb(diemWEB1);
+//# sourceMappingURL=index.js.map
---
+++ Lab 1/src/index.ts
@@ -0,0 +1,47 @@
+//BT 1
+let ten: string = "Định";
+let age: number = 21;
+let hienThi = `Xin chào, tôi là ${ten} và tôi ${age} tuổi.`
+
+// console.log(hienThi);
+
+//BT 2
+const num = 0.873457689;
+if (!isNaN(num)) {
+    // console.log("a không phải là số");
+}
+const convertNum = (num.toFixed(3)).toString()
+// console.log(convertNum, typeof convertNum);
+const convertNum1 = (num.toFixed(5)).toString()
+// console.log(num.toFixed(2), num.toFixed(5));
+
+//BT 3
+const sinhVien = "Sinh viên Công nghệ thông tin";
+const truongHoc = "FPT Polytechnic";
+// console.log(sinhVien.substring(0, 4), truongHoc.substring(0, 4));
+// console.log(sinhVien.concat(" - ", truongHoc));
+
+//BT 4
+const tinhTongHop = () : void => {    
+    const so1 = (document.getElementById("num1") as HTMLInputElement).valueAsNumber;
+    const so2 = (document.getElementById("num2") as HTMLInputElement).valueAsNumber;
+    const content = document.getElementById("content");
+    console.log(so1, so2);
+    
+    content!.innerHTML += 
+        `Tổng: ${so1+so2} <br> Hiệu: ${so1-so2} <br>
+        Tích: ${so1*so2} <br> Thương: ${so1/so2} <br>
+       `
+}
+
+//BT 5
+const diemWEB1: number[] = [6,7,9,10,5];
+
+const diemTb = (diems: number[]) => {
+    let tong: number= 0
+    for (const diem of diems) {
+        tong += diem
+    }
+    console.log("Điểm trung bình của lớp là: ",tong);
+}
+diemTb(diemWEB1)
